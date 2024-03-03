e4377ab6cb03a19831da0f85b5e216461e64e6e8
lewisev <lewisev@udel.edu>
2024-03-02 23:44:53 -0500

    Full level loading

--- levels.js
+++ levels.js
@@ -9,7 +9,17 @@ function setLevel(id) {
     window.location = window.location.pathname + '?level=' + id
 }
 
-LEVEL_ONE = `#color1 {
+const level = document.getElementById('levelname');
+const desc = document.getElementById('description');
+
+
+/*
+    LEVEL ONE
+*/
+
+LEVEL_ONE_TITLE = 'Level 1'
+
+LEVEL_ONE_CSS = `#color1 {
   background-color: red;
 }
 #color2 {
@@ -24,45 +34,80 @@ LEVEL_ONE = `#color1 {
 #color6 {
 }`
 
-LEVEL_TWO = `#color1 {
+LEVEL_ONE_DESC = `Welcome to Color Guru! For this first level, we are
+going to fill a color wheel using CSS. The first color, red, is done for you.
+<br /><br />
+<b>Primary Colors:</b> Red, Yellow, and Blue are the first group of colors by which all other
+colors on the color wheel are made from.
+<br /><br />
+<b>Secondary Colors:</b> Orange, Green, and Violet (Purple) are made by mixing two primary colors together.
+<br /><br />
+<b>Hint:</b> Go clockwise in ROYGBV order!`
+
+/*
+    LEVEL TWO
+*/
+
+LEVEL_TWO_TITLE = 'Level 2'
+
+LEVEL_TWO_CSS = `#color1 {
   background-color: red;
 }
-    
 #color2 {
   background-color: orange;
 }
-    
 #color3 {
   background-color: yellow;
 }
-    
 #color4 {
   background-color: green;
 }
-    
 #color5 {
   background-color: blue;
 }
-    
 #color6 {
   background-color: purple;
 }`
 
+LEVEL_TWO_DESC = `Welcome to HELL! For this first level, we are
+going to fill a color wheel using CSS. The first color, red, is done for you.
+<br /><br />
+<b>Primary Colors:</b> Red, Yellow, and Blue are the first group of colors by which all other
+colors on the color wheel are made from.
+<br /><br />
+<b>Secondary Colors:</b> Orange, Green, and Violet (Purple) are made by mixing two primary colors together.
+<br /><br />
+<b>Hint:</b> Go clockwise in ROYGBV order!`
+
+/*
+    --------
+*/
+
 function resetLevel(id) {
     console.log(id)
+
     editorText = `Error, loading first page...`
+    description = `Error loading data...`
+    title = `Error`
 
     switch (id) {
         case "1":
-            editorText = LEVEL_ONE
+            editorText = LEVEL_ONE_CSS
+            description = LEVEL_ONE_DESC
+            title = LEVEL_ONE_TITLE
             break;
         case "2":
-            editorText = LEVEL_TWO
+            editorText = LEVEL_TWO_CSS
+            description = LEVEL_TWO_DESC
+            title = LEVEL_TWO_TITLE
             break;
         default:
             setLevel(1)
 
     }
     editor.setValue(editorText)
+
+    level.innerText = title
+    desc.innerHTML = description
 
 }
